36fd966c13fdde39753aefccc9f6999bef5d71cf
yeqingyang <lpz8120903@163.com>
2015-01-27 16:47:53 +0800

    3

---
+++ wish/app/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+class IndexController extends ControllerBase 
+{
+	public function initialize() {
+		$this->tag->setTitle ( 'Home' );
+		parent::initialize ();
+	}
+
+	public function IndexAction(){
+		$this->view->setRenderLevel(\Phalcon\Mvc\View::LEVEL_MAIN_LAYOUT);
+	}
+
+}
